98d7a598fedd6625041c0c82854a3e2b0d046aee
Matthew Hale <FillipMatthew@gmail.com>
2022-07-26 22:20:54 +0200

    Removed an unused include

--- packages/tools_of_worship_server/lib/src/interfaces/fellowships_data_provider.dart
+++ packages/tools_of_worship_server/lib/src/interfaces/fellowships_data_provider.dart
@@ -1,5 +1,3 @@
-import 'package:tools_of_worship_server/src/types/access_level.dart';
-
 abstract class FellowshipsDataProvider {
   Future<void> getUserFellowships(String userId, int minAccessLevel, Map<String, String> userFellowships);
   Future<bool> exists(String name, String creatorId);
